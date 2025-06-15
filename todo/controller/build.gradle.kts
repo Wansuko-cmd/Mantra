dfc994df5f18dccb46a4084af79e5ba449bc39a7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-06-16 03:07:26 +0900

    [add] TODOのControllerモジュールを追加

---
+++ todo/controller/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(buildLogic.plugins.kotlin.multiplatform)
+}
+
+android {
+    namespace = "com.wsr.todo.controller"
+}
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(projects.todo.domain)
+                implementation(projects.todo.usecase)
+                implementation(projects.todo.repository)
+
+                implementation(libs.kotlin.coroutine)
+                implementation(libs.kotlin.datetime)
+            }
+        }
+    }
+}
